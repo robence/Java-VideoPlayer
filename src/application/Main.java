e043cd3479a6ae932c8cf03c85e8ee3430aeafb6
Bence Rózsa <Bence Rzsa>
2017-02-13 23:26:36 +0100

    second commit

---
+++ src/application/Main.java
@@ -0,0 +1,62 @@
+package application;
+	
+
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.paint.Color;
+
+
+public class Main extends Application {
+	
+	Player player;
+	FileChooser fileChooser;
+	public void start(final Stage primaryStage) {
+		
+		MenuItem open = new MenuItem("Open");
+		Menu file = new Menu("File");
+		MenuBar menu = new MenuBar();
+		
+		file.getItems().add(open);
+		menu.getMenus().add(file);
+		
+		fileChooser = new FileChooser();
+		
+		open.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent e){
+				player.player.pause();
+				File file = fileChooser.showOpenDialog(primaryStage);
+				if(file != null){
+					try {
+						player = new Player(file.toURI().toURL().toExternalForm());
+						Scene scene = new Scene(player, 720, 535, Color.BLACK);
+						primaryStage.setScene(scene);
+					} catch (MalformedURLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		player = new Player("file:///C:/trailer.mp4");
+		player.setTop(menu);
+		Scene scene = new Scene(player, 720, 535, Color.BLACK);
+		primaryStage.setScene(scene);
+		primaryStage.show(); 
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
